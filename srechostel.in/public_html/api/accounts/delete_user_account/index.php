9dd41b240eb1411ea0f5ef5b6935fcf11907bcf6
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-08-31 11:03:17 +0000

    s

--- srechostel.in/public_html/api/accounts/delete_user_account/index.php
+++ srechostel.in/public_html/api/accounts/delete_user_account/index.php
@@ -4,9 +4,21 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/" . "is-admin.php";
 
 # receive the input from the admin and see what kind of of inputs is received.
 
-if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    print_r(array_keys($_GET));
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $keys = array_keys($_POST);
+    if ($keys[0] == "roll_no") {
+
+    } else if ($keys[0] == "year") {
+
+    } else if (in_array("dept", $keys) and in_array("year", $keys)) {
+
+    } else if (in_array("group_of_roll_no", $keys)) {
+
+    } else {
+        header("Content-Type: application/json");
+        echo '{"error":"Wrong payload values"}';
+    }
 } else {
     header("Content-Type: application/json");
-    echo'{"error":"Method not allowed"}';
+    echo '{"error":"Method not allowed"}';
 }
