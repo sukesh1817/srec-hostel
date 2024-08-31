bd370ef284e1c2da83dc9110739da2bee5799e99
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-08-31 10:53:57 +0000

    s

--- srechostel.in/public_html/api/accounts/delete_user_account/index.php
+++ srechostel.in/public_html/api/accounts/delete_user_account/index.php
@@ -5,7 +5,7 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/" . "is-admin.php";
 # receive the input from the admin and see what kind of of inputs is received.
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    print_r($_GET);
+    print_r(array_keys($_GET));
 } else {
     header("Content-Type: application/json");
     echo'{"error":"Method not allowed"}';
