2ad63504172110098da6964816cc24f8f8151559
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-08-31 10:45:12 +0000

    sample commit after the some edit in api

--- srechostel.in/public_html/api/accounts/delete_user_account/index.php
+++ srechostel.in/public_html/api/accounts/delete_user_account/index.php
@@ -2,4 +2,5 @@
 # check the user is admin.
 include_once $_SERVER["DOCUMENT_ROOT"] . "/" . "is-admin.php";
 
-# 
+# receive the input from the admin and see what kind of of inputs is received.
+print_r($_SERVER);
