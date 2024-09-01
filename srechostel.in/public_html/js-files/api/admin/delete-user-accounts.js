aaa87efed5b65a5d8088dfbe39061f82edce9bed
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-01 06:04:23 +0000

    test

--- srechostel.in/public_html/js-files/api/admin/delete-user-accounts.js
+++ srechostel.in/public_html/js-files/api/admin/delete-user-accounts.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-    $("#single-form").submit(function (e) { 
+    const successToast = document.getElementById('toast-success');
+    const errorToast = document.getElementById('toast-error');
+
+    // Hide both toasts first
+    successToast.classList.remove('show');
+    successToast.classList.add('hidden');
+    errorToast.classList.remove('show');
+    errorToast.classList.add('hidden');
+
+    $("#single-form").submit(function (e) {
         e.preventDefault();
         $.ajax({
             type: "POST",
@@ -9,9 +18,27 @@ $(document).ready(function () {
             },
             // dataType: "json",
             success: function (data) {
-                console.log("done")
+                if (data['code'] == 1) {
+                    successToast.classList.remove('hidden');
+                    successToast.classList.add('show');
+                } else {
+                    errorToast.classList.remove('hidden');
+                    errorToast.classList.add('show');
+                }
+
+                setTimeout(() => {
+                    if (data["code"] == 1) {
+                        successToast.classList.remove('show');
+                        successToast.classList.add('hidden');
+                    } else if (data["code"] == 0) {
+                        errorToast.classList.remove('show');
+                        errorToast.classList.add('hidden');
+                    }
+                }, 3000);
             },
-            
+
         });
     });
 });
+
+
