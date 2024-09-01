e5ae5ae29a8e57746d2d0af6548a1f97f2a7bd11
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-01 04:27:21 +0000

    test

--- srechostel.in/public_html/api/accounts/delete_user_account/index.php
+++ srechostel.in/public_html/api/accounts/delete_user_account/index.php
@@ -4,10 +4,9 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/" . "is-admin.php";
 
 # receive the input from the admin and see what kind of of inputs is received.
 
-if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     include_once $_SERVER["DOCUMENT_ROOT"] . "/../class-files/" . "admin.class.php";
     $admin = new Admin();
-    print_r($_REQUEST);
     $keys = array_keys($_POST);
     $values = array_values($_POST);
     $resultValue = $admin->deleteUser($keys, $values);
--- srechostel.in/public_html/js-files/api/admin/delete-user-accounts.js
+++ srechostel.in/public_html/js-files/api/admin/delete-user-accounts.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("#single-form").submit(function (e) { 
         e.preventDefault();
         $.ajax({
-            type: "DELETE",
+            type: "POST",
             url: "/api/accounts/delete_user_account/",
             data: {
                 roll_no: $("#roll_no").val(),
