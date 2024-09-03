d733c6e768a50760db5758823a4a4fa93ce3ef30
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-03 13:06:54 +0000

    test

--- srechostel.in/public_html/logout/index.php
+++ srechostel.in/public_html/logout/index.php
@@ -5,7 +5,7 @@ In this we will delete the cookie 'SessId' and 'encrypt' and 'PHPSESSID'
 after that we will redirect the user to the login page
 */
 setcookie("SessId", "null", time() - 263000000, "/");
-setcookie("auth_user", "null", time() - 263000000, "/");
+setcookie("auth_session_id", "null", time() - 263000000, "/");
 setcookie("PHPSESSID", "bull", time() - 263000000, "/");
 setcookie("auth_watch_man", "bull", time() - 263000000, "/");
 session_destroy();
