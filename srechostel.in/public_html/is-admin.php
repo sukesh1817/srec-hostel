4ea97f4618a43cdfd14baa54a78d2d2dcf840ba5
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-07 02:10:03 +0000

    test

--- srechostel.in/public_html/is-admin.php
+++ srechostel.in/public_html/is-admin.php
@@ -4,20 +4,36 @@ This file helps to check wheather the person is admin
 if the person is admin allow them
 else do not allow 
 */
-if(isset($_COOKIE["auth_user"])) {
-    if($_COOKIE["auth_user"]==md5("auth_user")) {
-        if(isset($_SESSION["yourToken"])) {
-            
+
+if (isset($_COOKIE["auth_session_id"])) {
+    require_once $_SERVER["DOCUMENT_ROOT"] . "/" . "../class-files/connection.class.php";
+    $cookie = $_COOKIE["auth_session_id"];
+    $conn = new Connection();
+    $sqlConn = $conn->returnConn();
+    $sqlQuery = "SELECT admin_id,admin_session_id FROM `admin_session` WHERE admin_session_id='$cookie'";
+    if ($sqlConn->query($sqlQuery)) {
+        $result = $sqlConn->query($sqlQuery);
+        $row = $result->fetch_assoc();
+        if (isset($row["admin_session_id"])) {
+            if (isset($_SESSION["yourToken"])) {
+                //do nothing
+            } else {
+                session_start();
+                $_SESSION["yourToken"] = $row["admin_id"];
+            }
+
         } else {
-            session_start();
-            $_SESSION["yourToken"]=md5("auth_user") ;
+            $ip = $_SERVER["SERVER_ADDR"];
+            header("Location:  /");
+
         }
     } else {
-        header("Location: /");
-       
+        $ip = $_SERVER["SERVER_ADDR"];
+        header("Location:  /");
+    }
+} else {
+    $ip = $_SERVER["SERVER_ADDR"];
+    header("Location:  /");
 }
 
-} else {
-    header("Location: /");
-}
 
