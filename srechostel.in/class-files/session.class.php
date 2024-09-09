d9fcae2af67b9db2e6f4ada3ef9ea237a8066fc6
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-09 16:19:22 +0000

    testing_of_subdomains

--- srechostel.in/class-files/session.class.php
+++ srechostel.in/class-files/session.class.php
@@ -153,6 +153,8 @@ class session
 
             if ($sqlConn->query($sqlQuery)) {
                 $subdomain = strtolower($this->whoIs);
+                echo $subdomain;
+                exit;
                 setcookie("auth_session_id", $sessionId, time() + 2630000, "/", "$subdomain.srechostel.in", true, true);
                 return true;
             } else {
