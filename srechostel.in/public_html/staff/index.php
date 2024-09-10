e6fff2d06568579cc48e3c8af93ec918b5f428d6
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-10 08:48:39 +0000

    testing_sub_domain

---
+++ srechostel.in/public_html/staff/index.php
@@ -0,0 +1,5 @@
+<?php 
+include_once $_SERVER['DOCUMENT_ROOT']."/is-staff.php";
+// echo $_SERVER['DOCUMENT_ROOT'];
+echo "i am staff";
+?>
--- srechostel.in/public_html/staff/is-staff.php
+++ srechostel.in/public_html/staff/is-staff.php
@@ -4,17 +4,17 @@ This file helps to check wheather the person is staff
 if the person is staff allow them
 else do not allow 
 */
-if(isset($_COOKIE["SessId"])) {
-    include_once $_SERVER["DOCUMENT_ROOT"] ."/"."../class-files/connection.class.php";
+if (isset($_COOKIE["SessId"])) {
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/" . "../class-files/connection.class.php";
     $cookie = $_COOKIE["SessId"];
     $conn = new Connection();
     $sqlConn = $conn->returnConn();
     $sqlQuery = "SELECT staff_session_id,staff_id,sur_name FROM `staff_session` WHERE staff_session_id='$cookie'";
-    if($sqlConn->query($sqlQuery)) {
+    if ($sqlConn->query($sqlQuery)) {
         $result = $sqlConn->query($sqlQuery);
         $row = $result->fetch_assoc();
-        if(isset($row["staff_session_id"])) {
-            if(isset($_SESSION["yourToken"]) and isset($_SESSION['name'])) {
+        if (isset($row["staff_session_id"])) {
+            if (isset($_SESSION["yourToken"]) and isset($_SESSION['name'])) {
                 //do nothing
             } else {
                 session_start();
@@ -22,15 +22,17 @@ if(isset($_COOKIE["SessId"])) {
                 $_SESSION["name"] = $row["sur_name"];
             }
         } else {
-            $ip = $_SERVER["SERVER_ADDR"];
-            header("Location:  /");
+            do_redirection();
         }
     } else {
-        $ip = $_SERVER["SERVER_ADDR"];
-        header("Location:  /");
+        do_redirection();
     }
 } else {
+    do_redirection();
+}
+
+function do_redirection()
+{
     $ip = $_SERVER["SERVER_ADDR"];
     header("Location:  /");
 }
-
