a498171b3c274bbad5e1b98cb35bfef79369ba34
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-10 09:27:00 +0000

    testing_sub_domain

--- srechostel.in/public_html/student/token/index.php
+++ srechostel.in/public_html/student/token/index.php
@@ -2,105 +2,99 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Food token</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-<link  rel="icon" type="image/x-icon" href="/images/icons/food-icon.png">
-<style>
-  body {
-  font-family: "Poppins", sans-serif;
-  font-weight: 400;
-  font-style: normal;
-}
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food token</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
+    <?php
+    include_once $_SERVER['DOCUMENT_ROOT'] . "/__common/poppins.php";
+    ?>
 </head>
+
 <body>
-  <?php
-  include_once $_SERVER['DOCUMENT_ROOT'] . "/__common/navbar.php";
+    <?php
+    include_once $_SERVER['DOCUMENT_ROOT'] . "/__common/navbar.php";
+    ?>
+    <div class="container my-5" bis_skin_checked="1">
+        <div class="p-4 text-center bg-body-tertiary rounded-3" bis_skin_checked="1">
+            <svg class="bi mt-4 mb-3" style="color: var(--bs-indigo);" width="100" height="100">
+                <use xlink:href="#bootstrap"></use>
+            </svg>
+            <h1 class="text-body-emphasis">Hey Welcome
+                <?php if (isset($_SESSION['name'])) {
+                    echo $_SESSION['name'];
+                } else {
+                    echo "Student";
+                } ?>
+            </h1>
+            <p class="col-lg-8 mx-auto fs-5 text-muted">
+                Welcome! Book your food token easily on our site and check your booking status anytime under "Token
+                status."
+                Enjoy your meal!
+            </p>
+            <div class="gap-2 mb-5" bis_skin_checked="1">
+                <a class="align-items-center btn <?php
+                include_once($_SERVER["DOCUMENT_ROOT"] . "/../../class-files/token.class.php");
+                $token = new Token();
+                $result = $token->isTokenBooked($_SESSION["yourToken"]);
+                if (!(date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday")) {
+                    echo "btn-warning";
+                } else if ($result) {
+                    echo "btn-success";
+                } else {
+                    echo "btn-dark";
+                }
+                ?> rounded-1 mt-2" href="<?php
+                 if (date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday") {
+                     include_once($_SERVER["DOCUMENT_ROOT"] . "/" . "../class-files/token.class.php");
+                     $token = new Token();
+                     $result = $token->isTokenBooked($_SESSION["yourToken"]);
+                     if ($result) {
+                         echo "/stud-panel/token/";
+                     } else {
+                         echo "/stud-panel/token/book-token";
 
-  ?>
-  <div class="container my-5" bis_skin_checked="1">
-    <div class="p-4 text-center bg-body-tertiary rounded-3" bis_skin_checked="1">
-      <svg class="bi mt-4 mb-3" style="color: var(--bs-indigo);" width="100" height="100">
-        <use xlink:href="#bootstrap"></use>
-      </svg>
-      <h1 class="text-body-emphasis">Hey Welcome
-        <?php if (isset($_SESSION['name'])) {
-          echo $_SESSION['name'];
-        } else {
-          echo "Student";
-        } ?> </h1>
-      <p class="col-lg-8 mx-auto fs-5 text-muted">
-        Welcome! Book your food token easily on our site and check your booking status anytime under "Token status."
-        Enjoy your meal!
-      </p>
-      <div class="gap-2 mb-5" bis_skin_checked="1">
-        <a class="align-items-center btn <?php
-        include_once ($_SERVER["DOCUMENT_ROOT"] . "/" . "../class-files/token.class.php");
-        $token = new Token();
-        $result = $token->isTokenBooked($_SESSION["yourToken"]);
-        if (!(date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday")) {
-          echo "btn-warning";
-        }
-        else if($result){
-          echo "btn-success";
-        } else {
-          echo "btn-dark";
-        } 
-         ?> rounded-1 mt-2" href="<?php
-        if (date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday") {
-          include_once ($_SERVER["DOCUMENT_ROOT"] . "/" . "../class-files/token.class.php");
-          $token = new Token();
-          $result = $token->isTokenBooked($_SESSION["yourToken"]);
-          if($result){
-            echo "/stud-panel/token/";
-          } else {
-            echo "/stud-panel/token/book-token";
+                     }
+                 } else {
+                     echo "/stud-panel/token/";
+                 }
+                 ?>" type="button">
+                    <?php
+                    if (date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday") {
+                        include_once($_SERVER["DOCUMENT_ROOT"] . "/" . "../class-files/token.class.php");
+                        $token = new Token();
+                        $result = $token->isTokenBooked($_SESSION["yourToken"]);
+                        if ($result) {
+                            echo "Token booked";
+                        } else {
+                            echo "Book token";
 
-          }
-        } else {
-          echo "/stud-panel/token/";
-        }
-        ?>" type="button">
-          <?php
-          if (date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday") {
-            include_once ($_SERVER["DOCUMENT_ROOT"] . "/" . "../class-files/token.class.php");
-            $token = new Token();
-            $result = $token->isTokenBooked($_SESSION["yourToken"]);
-            if($result){
-              echo "Token booked";
-            } else {
-              echo "Book token";
+                        }
+                    } else {
+                        echo "Booking not opened";
+                    }
 
-            }
-          } else {
-            echo "Booking not opened";
-          }
+                    ?>
 
-          ?>
-
-        </a>
-        <br>
-        <a class="align-items-center btn btn-outline-secondary rounded-1 mt-2" href="<?php
-        if (date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday") {
-          echo "/stud-panel/token/token-status";
-        } else {
-          echo "/stud-panel/token/";
-        }
-        ?>" type="button">
-          Token status
-        </a>
-      </div>
+                </a>
+                <br>
+                <a class="align-items-center btn btn-outline-secondary rounded-1 mt-2" href="<?php
+                if (date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday") {
+                    echo "/stud-panel/token/token-status";
+                } else {
+                    echo "/stud-panel/token/";
+                }
+                ?>" type="button">
+                    Token status
+                </a>
+            </div>
+        </div>
     </div>
-  </div>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
