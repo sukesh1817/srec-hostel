e2e9648c34e776e2b5797101128662faa19a4f51
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-10 16:52:43 +0000

    cdn edit

--- srechostel.in/public_html/student/token/edit-token/index.php
+++ srechostel.in/public_html/student/token/edit-token/index.php
@@ -15,8 +15,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/is-student.php";
   <title>Edit Food Token</title>
   <link rel="icon" type="image/x-icon" href="/images/icons/food-icon.png">
 
-  <link href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
   <?php
   // poppins font css included.
@@ -418,9 +417,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/is-student.php";
   }
 
   ?>
-  <script src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- srechostel.in/public_html/student/token/token-status/index.php
+++ srechostel.in/public_html/student/token/token-status/index.php
@@ -12,10 +12,9 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/is-student.php";
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.122.0">
     <title>Food Token Status</title>
     <link rel="icon" type="image/x-icon" href="/images/icons/food-icon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <?php
     // poppins font css included.
     require_once $_SERVER['DOCUMENT_ROOT'] . "/__common/poppins.php";
@@ -281,9 +280,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/is-student.php";
     ?>
 
 
-    <script src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
