49ab132677e95efa6f8ecc6b5e1af9e1e5da8966
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-10 08:14:41 +0000

    testing_sub_domain

--- srechostel.in/public_html/api/auth/logout/index.php
+++ srechostel.in/public_html/api/auth/logout/index.php
@@ -4,12 +4,11 @@ this is logout source code
 In this we will delete the cookie 'SessId' and 'encrypt' and 'PHPSESSID'
 after that we will redirect the user to the login page
 */
-setcookie("SessId", "null", time() - 263000000, "/","student.srechostel.in");
-setcookie("auth_session_id", "null", time() - 263000000, "/", "mens-1.srechostel.in");
-setcookie("auth_session_id", "null", time() - 263000000, "/", "mens-2.srechostel.in");
-setcookie("auth_session_id", "null", time() - 263000000, "/", "women.srechostel.in");
-setcookie("PHPSESSID", "null", time() - 263000000, "/", "student.srechostel.in");
-setcookie("auth_watch_man", "null", time() - 263000000, "/", "student.srechostel.in");
+setcookie("SessId", "null", time() - 263000000, "/","srechostel.in");
+setcookie("auth_session_id", "null", time() - 263000000, "/", "srechostel.in");
+setcookie("auth_session_id", "null", time() - 263000000, "/", "srechostel.in");
+setcookie("PHPSESSID", "null", time() - 263000000, "/", "srechostel.in");
+setcookie("auth_watch_man", "null", time() - 263000000, "/", "srechostel.in");
 session_destroy();
 sleep(2);
 header("Location: /");
