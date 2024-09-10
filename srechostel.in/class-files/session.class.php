fa99f0bc9f417da04fc7d05c8f6cd24ec33ef400
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-10 08:10:04 +0000

    testing_sub_domain

--- srechostel.in/class-files/session.class.php
+++ srechostel.in/class-files/session.class.php
@@ -102,8 +102,7 @@ class session
 
             if ($sqlConn->query($sqlQuery)) {
                 $subdomain = strtolower($this->whoIs);
-                
-                setcookie("auth_session_id", $sessionId, time() + 2630000, "/", "$subdomain.srechostel.in", true, true);
+                setcookie("auth_session_id", $sessionId, time() + 2630000, "/", "$subdomain.srechostel.in");
                 return true;
             }
         } else if ($this->whoIs == "Watch-man-1") {
