e95961130f5f200de3a3a49214878dc74aed2289
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-10 09:24:19 +0000

    testing_sub_domain

--- srechostel.in/public_html/student/token/index.php
+++ srechostel.in/public_html/student/token/index.php
@@ -41,7 +41,7 @@
       </p>
       <div class="gap-2 mb-5" bis_skin_checked="1">
         <a class="align-items-center btn <?php
-        include_once ($_SERVER["DOCUMENT_ROOT"] . "/" . "../../class-files/token.class.php");
+        include_once ($_SERVER["DOCUMENT_ROOT"] . "/" . "../class-files/token.class.php");
         $token = new Token();
         $result = $token->isTokenBooked($_SESSION["yourToken"]);
         if (!(date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday")) {
@@ -69,7 +69,7 @@
         ?>" type="button">
           <?php
           if (date("l") == "Tuesday" or date("l") == "Wednesday" or date("l") == "Thursday" or date("l") == "Friday" or date("l") == "Saturday") {
-            include_once ($_SERVER["DOCUMENT_ROOT"] . "/" . "../../class-files/token.class.php");
+            include_once ($_SERVER["DOCUMENT_ROOT"] . "/" . "../class-files/token.class.php");
             $token = new Token();
             $result = $token->isTokenBooked($_SESSION["yourToken"]);
             if($result){
@@ -98,6 +98,7 @@
       </div>
     </div>
   </div>
+
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     crossorigin="anonymous"></script>
