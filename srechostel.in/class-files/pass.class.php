2eeebf0966b53d1c7e76737bae04c285ab5ca7d2
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-11 15:08:40 +0000

    ls

--- srechostel.in/class-files/pass.class.php
+++ srechostel.in/class-files/pass.class.php
@@ -89,6 +89,9 @@ class Pass_class
             $file = $array['file'];
             $file_name = md5($rollNo) . '.' . pathinfo($file['name'], PATHINFO_EXTENSION);
             $tmp = $file['tmp_name'];
+            chdir();
+            echo getcwd();
+            system("ls");
             $dir = $_SERVER["DOCUMENT_ROOT"]."/../../".'files/student-files/working-day-auth-letter/' . $file_name;
     
             if (!move_uploaded_file($tmp, $dir)) {
