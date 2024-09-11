f59636aaced9858831c171a0a2187664538bd672
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-11 10:59:16 +0000

    m-2

--- srechostel.in/public_html/student/__common/__breadcrumbs/bookpass.php
+++ srechostel.in/public_html/student/__common/__breadcrumbs/bookpass.php
@@ -1,4 +1,4 @@
-<nav aria-label="breadcrumb">
+<nav aria-label="breadcrumb" class="mt-2 ms-2">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="/">Home</a></li>
     <li class="breadcrumb-item"><a href="/gate-pass/">Gate pass</a></li>
