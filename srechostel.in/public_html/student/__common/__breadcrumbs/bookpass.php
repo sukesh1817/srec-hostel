bc5cb01d2c085cac3dc5b668ec7f41a5e70667fb
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-12 16:20:33 +0000

    bread crumbs

--- srechostel.in/public_html/student/__common/__breadcrumbs/bookpass.php
+++ srechostel.in/public_html/student/__common/__breadcrumbs/bookpass.php
@@ -6,7 +6,7 @@ function bread_crumb_gatepass($current_page)
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="/">Home</a></li>
       <li class="breadcrumb-item"><a href="/gate-pass/">Gate pass</a></li>
-      <li class="breadcrumb-item active" aria-current="page"><?php $current_page ?></li>
+      <li class="breadcrumb-item active" aria-current="page"><?php echo $current_page ?></li>
     </ol>
   </nav>
   <?php
