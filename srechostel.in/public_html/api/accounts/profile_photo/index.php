b718078509cbb7c62dcdf9ae197cb670ad9065bd
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-13 23:25:49 +0000

    test

--- srechostel.in/public_html/api/accounts/profile_photo/index.php
+++ srechostel.in/public_html/api/accounts/profile_photo/index.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
+echo "hello";
+
 if (isset($_SESSION['yourToken'])) {
     // header("Content-Type: image/jpg");
     $rollNo = $_SESSION['yourToken'];
-    echo "hello";
     echo $_SERVER['DOCUMENT_ROOT'];
     if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/../profile-photos/" . $rollNo . '.jpg')) {
         include_once $_SERVER['DOCUMENT_ROOT'] . "/" . "../profile-photos/" . $rollNo . '.jpg';
