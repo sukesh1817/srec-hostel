c9b6d5cb7011b7979bca99936110ccdb7143196c
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-13 13:56:41 +0000

    test

--- srechostel.in/public_html/student/profile/edit-profile/index.php
+++ srechostel.in/public_html/student/profile/edit-profile/index.php
@@ -126,7 +126,7 @@ use Intervention\Image\Drivers\Gd\Driver;
                     if (file_exists($_SESSION['yourToken'] . $ext)) {
                         unlink($_SESSION['yourToken'] . $ext);
                     }
-                    // chdir($_SERVER['DOCUMENT_ROOT'] . "/../profile-photos/");
+                    chdir($_SERVER['DOCUMENT_ROOT'] . "/../profile-photos/tmp/");
                 }
                 $dir = $_SESSION['yourToken'] . '.heic';
                 if (move_uploaded_file($_FILES["profile-img"]["tmp_name"], $dir)) {
