1150e07e37a55ca42a70bc2658ddb35bcad8e4c8
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-21 03:34:51 +0000

    test

--- srechostel.in/public_html/student/complaint/book-individual-complaint/index.php
+++ srechostel.in/public_html/student/complaint/book-individual-complaint/index.php
@@ -1,11 +1,60 @@
+<?php
+// check the login user is student
+include_once $_SERVER['DOCUMENT_ROOT'] . "/is-student.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>book individual complaint</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+    // poppins font css included.
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/__common/poppins.php";
+    ?>
+    <style>
+        body {
+            font-family: "Poppins", sans-serif;
+            font-style: normal;
+            font-weight: 500;
+            /* font-size: 40px; */
+
+        }
+
+        .cont {
+            max-width: 600px;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+        }
+
+        textarea {
+            resize: none;
+        }
+    </style>
+
+</head>
+
 <body>
 
     <?php
-    include_once $_SERVER['DOCUMENT_ROOT'] . "/../template/student-template/common-template/navbar.php";
-    ?>
-    <?php
-    include_once ($_SERVER["DOCUMENT_ROOT"] . "/../class-files/common.class.php");
+
+    // navbar html code is included.
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/__common/navbar.php";
+
+    // common class is included.
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/../../class-files/common.class.php");
+    
+    // common class is initialized.
     $complaint = new commonClass();
+
+    // to find which compalint is booked.
     $which_is_booked = $complaint->alreadyBooked($_SESSION['yourToken']);
     if ($which_is_booked) {
         ?>
@@ -58,7 +107,6 @@
                 <div class="container my-5" bis_skin_checked="1">
                     <div class="position-relative p-5 text-center text-muted bg-body border border-dashed  rounded-1"
                         bis_skin_checked="1">
-
                         <svg class="bi mt-5 mb-3" width="48" height="48">
                             <use xlink:href="#check2-circle"></use>
                         </svg>
@@ -71,44 +119,8 @@
                         </a>
                     </div>
                 </div>
-                <div class="notification rounded-1" role="alert" aria-live="assertive" aria-atomic="true">
-                    <div class="notification-header">
-                        <h3 class="notification-title">Notification</h3>
-                        <i type="button" class="btn-close-1" data-bs-dismiss="notification" aria-label="Close"><svg
-                                xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                            </svg></i>
-                    </div>
-                    <div class="notification-container">
-                        <div class="notification-media">
-                            <img src="https://blog.tryshiftcdn.com/uploads/2021/01/notifications@2x.jpg" alt=""
-                                class="notification-user-avatar">
-                            <i class="fa fa-thumbs-up notification-reaction"></i>
-                        </div>
-                        <div class="notification-content">
-                            <p class="notification-text">
-                                <strong>Your complaint booked successfully</strong>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <script>
-                    document.addEventListener('DOMContentLoaded', (event) => {
-                        const closeButton = document.querySelector('.btn-close-1');
-                        const notification = document.querySelector('.notification');
-
-                        closeButton.addEventListener('click', () => {
-                            notification.classList.add('hidden');
-                        });
-                    });
-                </script>
             <?php
         }
-
-
     } else {
 
         ?>
@@ -117,13 +129,8 @@
 
                     <h2 class="text-center">Individual Complaint</h2>
                     <hr>
-                    <form class="needs-validation" action="/stud-panel/complaint/book-individual-complaint/" method="post"
+                    <form class="needs-validation" action="/complaint/book-individual-complaint/" method="post"
                         enctype="multipart/form-data">
-
-                        <!-- 
-                    <input class='form-control mb-2' id="dept" name="dept" type="text" required /> -->
-
-
                         <label class='form-label' class="form-label" for="complaint_details">Breif details about your
                             complaint</label>
                         <textarea class='form-control mb-2' id="complaint_details" name="complaint_details" rows="4" cols="50"
@@ -132,18 +139,16 @@
                         <label class='form-label' class="form-label" for="evidence">Attach File</label>
                         <input class='form-control mb-2' class="form-control" type="file" accept=".jpg,.png,.jpeg,.heic"
                             id="evidence" name="evidence" required>
-
                         <button class="btn btn-dark container-fluid mt-3" class="form-control" type="submit">Submit</button>
                     </form>
                 </div>
-
             </div>
-
         <?php
-
     }
     ?>
-    <?php
-    // include_once $_SERVER['DOCUMENT_ROOT'] . "/../template/student-template/common-template/footbar.php";
-    ?>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+
+</html>
