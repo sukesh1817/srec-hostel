d6570c7c46a9840d6db4f31b2647e37355e8977a
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-09-23 05:35:42 +0000

    loader

--- srechostel.in/public_html/index.php
+++ srechostel.in/public_html/index.php
@@ -204,7 +204,7 @@ if (isset($_COOKIE['SessId'])) {
         </div>
     </section>
 
-    <div id="loading_indicator" class="spinner-border" role="status">
+    <div id="loading_indicator" class="spinner-border p-4" role="status">
             <span class="sr-only">Loading...</span>
         </div>
 
