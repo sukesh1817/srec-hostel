b823e3a8be38bc8fd6db0fa5d83b48a53399c2d5
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-10-04 17:14:54 +0000

    changes-in-women-admin

--- srechostel.in/public_html/women-admin/student-details/index.php
+++ srechostel.in/public_html/women-admin/student-details/index.php
@@ -173,6 +173,32 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/is-women-admin.php';
             display: block;
             /* Show if error is active */
         }
+
+
+        .dropdown-item:hover {
+            background-color: #e2e6ea;
+            /* Lighten the hover color */
+        }
+
+        .btn-danger {
+            border: none;
+            background: none;
+            /* Remove default button background */
+            color: red;
+            /* Use a clear color for removal */
+            cursor: pointer;
+        }
+
+        .btn-danger:hover {
+            color: darkred;
+            /* Change color on hover */
+        }
+
+        #selectedYear,
+        #selectedDepartment {
+            font-weight: bold;
+            /* Emphasize selected text */
+        }
     </style>
 
 
@@ -202,71 +228,68 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/is-women-admin.php';
     </div><br>
 
     <div class="bg-light-subtle py-4">
-        <div class="container mt-2 text-center">
-            <div class="dropdown d-inline-block">
-                <button class="btn btn-dark rounded-1 dropdown-toggle" type="button" id="dropdownMenuButton"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    Search by
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <li class="dropdown-submenu">
+    <div class="container mt-2 text-center">
+        <div class="dropdown d-inline-block">
+            <button class="btn btn-dark rounded-1 dropdown-toggle" type="button" id="dropdownMenuButton"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Search by
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li class="dropdown-submenu">
                     <a class="dropdown-item dropdown-toggle" href="#">Year</a>
-                        <ul class="dropdown-menu">
-                            <li><a id="year-1" class="dropdown-item i" href="#">1</a></li>
-                            <li><a id="year-2" class="dropdown-item i" href="#">2</a></li>
-                            <li><a id="year-3" class="dropdown-item i" href="#">3</a></li>
-                            <li><a id="year-4" class="dropdown-item i" href="#">4</a></li>
-                        </ul>
-                    </li>
-    
-                    <li class="dropdown-submenu">
-                        <a class="dropdown-item dropdown-toggle" href="#">Department</a>
-                        <ul class="dropdown-menu">
-                            <li><a id="dept-ai" class="dropdown-item i" href="#">B.Tech AIDS</a></li>
-                            <li><a id="dept-it" class="dropdown-item i" href="#">B.Tech IT</a></li>
-                            <li><a id="dept-ece" class="dropdown-item i" href="#">B.E ECE</a></li>
-                            <li><a id="dept-eee" class="dropdown-item i" href="#">B.E EEE</a></li>
-                            <li><a id="dept-mech" class="dropdown-item i" href="#">B.E MECH</a></li>
-                            <li><a id="dept-bme" class="dropdown-item i" href="#">B.E BME</a></li>
-                            <li><a id="dept-metch" class="dropdown-item i" href="#">M.Tech CSE</a></li>
-                            <li><a id="dept-civil" class="dropdown-item i" href="#">B.E CIVIL</a></li>
-                            <li><a id="dept-aero" class="dropdown-item i" href="#">B.E AERO</a></li>
-                            <li><a id="dept-ra" class="dropdown-item i" href="#">B.E RA</a></li>
-                            <li><a id="dept-cse" class="dropdown-item i" href="#">B.E CSE</a></li>
-                            <li><a id="dept-eie" class="dropdown-item i" href="#">B.E EIE</a></li>
-                            <li><a id="dept-mba" class="dropdown-item i" href="#">B.E MBA</a></li>
-
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-
-            <form id="search-form" action="/student-details/" method="post" class="mt-3 d-inline-block">
-                <input type="hidden" name="year" id="yearValue">
-                <input type="hidden" name="department" id="departmentValue">
-                <input type="hidden" name="hostel" value="<?php echo $_POST['hostel'] ?? 'men-hostel-1'; ?>">
-
-                <div class="card p-man d-inline-flex">
-                    <div class="card-body p-man d-flex align-items-center">
-                        <p class="card-text mb-0 p-man">
-                            <span id="selectedYear">Year - NULL</span>
-                            <button type="button" id="removeYear" class="btn">
-                                <!-- SVG X -->
-                            </button>
-                        </p>
-                        <p class="card-text mb-0 ms-3">
-                            <span id="selectedDepartment">Department - NULL</span>
-                            <button type="button" id="removeDepartment" class="btn">
-                                <!-- SVG X -->
-                            </button>
-                        </p>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-outline-dark mt-3" id="submitButton" disabled>Search</button>
-            </form>
+                    <ul class="dropdown-menu">
+                        <li><a id="year-1" class="dropdown-item" href="#">1</a></li>
+                        <li><a id="year-2" class="dropdown-item" href="#">2</a></li>
+                        <li><a id="year-3" class="dropdown-item" href="#">3</a></li>
+                        <li><a id="year-4" class="dropdown-item" href="#">4</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item dropdown-toggle" href="#">Department</a>
+                    <ul class="dropdown-menu">
+                        <li><a id="dept-ai" class="dropdown-item" href="#">B.Tech AIDS</a></li>
+                        <li><a id="dept-it" class="dropdown-item" href="#">B.Tech IT</a></li>
+                        <li><a id="dept-ece" class="dropdown-item" href="#">B.E ECE</a></li>
+                        <li><a id="dept-eee" class="dropdown-item" href="#">B.E EEE</a></li>
+                        <li><a id="dept-mech" class="dropdown-item" href="#">B.E MECH</a></li>
+                        <li><a id="dept-bme" class="dropdown-item" href="#">B.E BME</a></li>
+                        <li><a id="dept-metch" class="dropdown-item" href="#">M.Tech CSE</a></li>
+                        <li><a id="dept-civil" class="dropdown-item" href="#">B.E CIVIL</a></li>
+                        <li><a id="dept-aero" class="dropdown-item" href="#">B.E AERO</a></li>
+                        <li><a id="dept-ra" class="dropdown-item" href="#">B.E RA</a></li>
+                        <li><a id="dept-cse" class="dropdown-item" href="#">B.E CSE</a></li>
+                        <li><a id="dept-eie" class="dropdown-item" href="#">B.E EIE</a></li>
+                        <li><a id="dept-mba" class="dropdown-item" href="#">B.E MBA</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
-    </div>
 
+        <form id="search-form" action="/student-details/" method="post" class="mt-3 d-inline-block">
+            <input type="hidden" name="year" id="yearValue">
+            <input type="hidden" name="department" id="departmentValue">
+            <input type="hidden" name="hostel" value="<?php echo $_POST['hostel'] ?? 'men-hostel-1'; ?>">
+
+            <div class="card p-man d-inline-flex mb-3">
+                <div class="card-body p-man d-flex align-items-center">
+                    <p class="card-text mb-0 p-man me-3">
+                        <span id="selectedYear">Year - NULL</span>
+                        <button type="button" id="removeYear" class="btn btn-danger btn-sm ms-2">
+                            <!-- SVG X -->
+                        </button>
+                    </p>
+                    <p class="card-text mb-0 p-man">
+                        <span id="selectedDepartment">Department - NULL</span>
+                        <button type="button" id="removeDepartment" class="btn btn-danger btn-sm ms-2">
+                            <!-- SVG X -->
+                        </button>
+                    </p>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-outline-dark mt-3" id="submitButton" disabled>Search</button>
+        </form>
+    </div>
+</div>
 
 
 
