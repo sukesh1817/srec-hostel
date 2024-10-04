09a879f5a1358493b94987c2f270c00aab08eac8
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-10-04 15:01:40 +0000

    ui-test-index

--- srechostel.in/public_html/index.php
+++ srechostel.in/public_html/index.php
@@ -238,7 +238,7 @@ if (isset($_COOKIE['SessId'])) {
 
     <div id="loading_indicator">
         <svg viewBox="25 25 50 50">
-            <circle r="20" cy="50" cx="50" fill="#ffff"></circle>
+            <circle r="20" cy="50" cx="50" ></circle>
         </svg>
     </div>
 
