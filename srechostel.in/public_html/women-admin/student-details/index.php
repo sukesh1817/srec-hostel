ea8a1c65d75de279a4c2c104d85bb8a72f2645bf
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-10-05 04:53:13 +0000

    women-admin-search-students

--- srechostel.in/public_html/women-admin/student-details/index.php
+++ srechostel.in/public_html/women-admin/student-details/index.php
@@ -19,7 +19,6 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/is-women-admin.php';
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="/css-files/toast.css">
     <?php
     // included the poppins font.
     include_once $_SERVER['DOCUMENT_ROOT'] . "/__common/poppins.php";
