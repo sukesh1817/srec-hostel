5b5ddb8dc4034ea3a4f8919d8e8a04c98f591dbc
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-10-05 04:50:20 +0000

    women-admin-search-students

--- srechostel.in/public_html/api/admin/search_student/index.php
+++ srechostel.in/public_html/api/admin/search_student/index.php
@@ -1,5 +1,8 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");  // Allow all origins
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json');
 include_once $_SERVER["DOCUMENT_ROOT"] . '/..' . "/class-files/api/admin.class.php";
 
