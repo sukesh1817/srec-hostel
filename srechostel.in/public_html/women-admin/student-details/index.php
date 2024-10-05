b901b45e8c84a7466075ef43f31c6e61eb9fe0f9
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-10-05 05:19:55 +0000

    women-admin-search-students

--- srechostel.in/public_html/women-admin/student-details/index.php
+++ srechostel.in/public_html/women-admin/student-details/index.php
@@ -260,7 +260,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/is-women-admin.php';
                 <input type="hidden" name="year" id="yearValue">
                 <input type="hidden" name="department" id="departmentValue">
 
-                <div style="width: 80%" class="dropdown">
+                <div style="width: 200%" class="dropdown">
                     <input type="text" id="searchQueryInput" class="form-control" placeholder="Search..."
                         aria-label="Search">
                     <ul id="myUL">
