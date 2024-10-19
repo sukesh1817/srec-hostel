9a2b22f74b93efd6f806fb995cb0d39f988ef7d2
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-10-19 15:01:39 +0000

    gate_pass_change

--- srechostel.in/class-files/api/admin.class.php
+++ srechostel.in/class-files/api/admin.class.php
@@ -239,10 +239,10 @@ class Admin
                 $sql = "SELECT g.*, s.year_of_study FROM gate_pass g JOIN stud_details s ON g.roll_no = s.roll_no WHERE 1=1";
                 break;
             case 2:
-                $sql = "SELECT w.*, s.year_of_study FROM working_days_pass w JOIN stud_details s ON w.roll_no = s.roll_no WHERE 1=1";
+                $sql = "SELECT g.*, s.year_of_study FROM general_home_pass g JOIN stud_details s ON g.roll_no = s.roll_no WHERE 1=1";
                 break;
             case 3:
-                $sql = "SELECT g.*, s.year_of_study FROM general_home_pass g JOIN stud_details s ON g.roll_no = s.roll_no WHERE 1=1";
+                $sql = "SELECT w.*, s.year_of_study FROM working_days_pass w JOIN stud_details s ON w.roll_no = s.roll_no WHERE 1=1";
                 break;
             default:
                 return ['error' => 'Invalid pass type'];
