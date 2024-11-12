3ce28e7b33342f587cdedafc3b02b628f1fcfa61
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-11-12 10:47:12 +0000

    complaint_test

--- srechostel.in/public_html/women-admin/complaint/index.php
+++ srechostel.in/public_html/women-admin/complaint/index.php
@@ -24,7 +24,7 @@ function echo_main_template()
     require_once $_SERVER['DOCUMENT_ROOT'] . '/../../config/' . "domain.php";
     ?>
     <div class="px-4 py-5 my-5 text-center">
-        <img class="d-block mx-auto mb-4" src="https://<?php echo $domain; ?>/images/icons/complaint.jpg" alt="" width="72"
+        <img class="d-block mx-auto mb-4" src="<?php echo $domain; ?>/images/icons/complaint.jpg" alt="" width="72"
             height="57">
         <h1 class="display-5 fw-bold text-body-emphasis">Complaint page</h1>
         <div class="col-lg-6 mx-auto">
