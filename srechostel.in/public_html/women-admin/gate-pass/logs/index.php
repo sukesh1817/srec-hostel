b28671e573130a81210ace0e53abc43dfdbdca0e
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-11-12 12:51:55 +0000

    test

--- srechostel.in/public_html/women-admin/gate-pass/logs/index.php
+++ srechostel.in/public_html/women-admin/gate-pass/logs/index.php
@@ -306,7 +306,6 @@ function get_student_entry_logs($which_hostel = "women_hostel_entry_log", $count
                             <td><?php echo $logs[$temp]['approved_warden'] ?></td>
                             <td><?php echo $logs[$temp]['approved_watch_man'] ?></td>
                             <td><?php echo $logs[$temp]['time_of_approval_by_warden'] ?></td>
-                            <td><?php echo $logs[$temp]['time_of_entry_by_watch_man'] ?></td>
                             <td>
                                 <?php if ($logs[$temp]['time_of_entry_by_watch_man'] == null) {
                                     echo "-";
