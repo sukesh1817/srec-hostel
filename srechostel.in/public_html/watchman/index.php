113a6bfaf76941249502af0181ac45dcf0a6170e
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-11-21 11:32:43 +0000

    watch_panel_correction

--- srechostel.in/public_html/watchman/index.php
+++ srechostel.in/public_html/watchman/index.php
@@ -16,7 +16,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/../../config/' . "domain.php";
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<link rel="stylesheet" href="/css-files/scan.css">
+	<link rel="stylesheet" href="<?php echo $domain; ?>/css-files/scan.css">
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 	<title>QR Entry</title>
