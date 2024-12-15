3758f72804fdd8ed546c3529c83da75df1e46c71
sukesh1817 <145757302+sukesh1817@users.noreply.github.com>
2024-12-15 10:09:24 +0000

    watch_panel_correction

--- srechostel.in/public_html/api/watch_man/entry/index.php
+++ srechostel.in/public_html/api/watch_man/entry/index.php
@@ -6,7 +6,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
+echo $_SERVER['DOCUMENT_ROOT'];
 
 // include_once $_SERVER['DOCUMENT_ROOT'] . "/is-watch-man.php";
 
